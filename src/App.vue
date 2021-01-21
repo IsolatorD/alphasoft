3d66a233f584a077523ec927b96356b15f4f7083
Daniel Rodriguez <daniel.rddev@gmail.com>
2021-01-21 14:11:55 -0400

    menu

--- src/App.vue
+++ src/App.vue
@@ -20,36 +20,44 @@
       </div>
 
       <v-spacer></v-spacer>
+      <v-toolbar-items v-if="$vuetify.breakpoint.smAndDown">
+        <v-menu
+          bottom
+          left
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-menu</v-icon>
+            </v-btn>
+          </template>
 
-      <v-btn
-        to="/"
-        class="py-8"
-        text
-      >
-        <span class="mr-2">Inicio</span>
-      </v-btn>
-      <v-btn
-        to="/about"
-        class="py-8"
-        text
-      >
-        <span class="mr-2">Sobre Nosotros</span>
-      </v-btn>
-      <v-btn
-        to="/blog"
-        class="py-8"
-        text
-      >
-        <span class="mr-2">Blog</span>
-      </v-btn>
-      <!-- <v-btn
-        class="py-8"
-        text
-      >
-        <span class="mr-2">Contactanos</span>
-      </v-btn> -->
+          <v-list>
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+              :to="item.to"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
+      <v-toolbar-items v-else>
+        <v-btn
+          v-for="(item, j) in items"
+          :key="j"
+          :to="item.to"
+          class="py-8"
+          text
+        >
+          <span class="mr-2">{{ item.title }}</span>
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
-
     <v-main>
       <transition name="fade">
         <router-view/>
@@ -60,7 +68,14 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({
+    items: [
+      {title: 'Inicio', to: '/'},
+      {title: 'Sobre Nosotros', to: '/about'},
+      {title: 'Blog', to: '/blog'},
+    ]
+  })
 };
 </script>
 
